1d094372c39ff5085ca241d58f4c8819c1293632
Đỗ Ngọc Minh Tiến <tien.dnm@outlook.com>
2024-05-18 11:47:11 +0700

    add some packages

--- src/lib/notion/notion-utils/normalize-url.ts
+++ src/lib/notion/notion-utils/normalize-url.ts
@@ -1,7 +1,7 @@
-import mem from "mem";
+import memoize from "memoize";
 import normalizeUrlImpl from "normalize-url";
 
-export const normalizeUrl = mem((url?: string) => {
+export const normalizeUrl = memoize((url?: string) => {
   if (!url) {
     return "";
   }
@@ -9,7 +9,7 @@ export const normalizeUrl = mem((url?: string) => {
   try {
     if (url.startsWith("https://www.notion.so/image/")) {
       const u = new URL(url);
-      const subUrl = decodeURIComponent(u.pathname.substr("/image/".length));
+      const subUrl = decodeURIComponent(u.pathname.substring("/image/".length));
       const normalizedSubUrl = normalizeUrl(subUrl);
       u.pathname = `/image/${encodeURIComponent(normalizedSubUrl)}`;
       url = u.toString();
